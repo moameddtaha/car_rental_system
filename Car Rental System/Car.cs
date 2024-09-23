abd807cc997e096d00cfb7504851010defa9dfa0
Mohamed Taha <mtaha9602@gmail.com>
2024-09-23 12:48:32 +0300

    init

---
+++ Car Rental System/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_System
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double RentalPrice { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+        public Car() { }
+
+        public Car(int id, string make, string model, double rentalPrice, bool isAvailable)
+        {
+            Id = id;
+            Make = make;
+            Model = model;
+            RentalPrice = rentalPrice;
+            IsAvailable = isAvailable;
+        }
+
+        public bool Rent()
+        {
+            if (IsAvailable)
+            {
+                Console.WriteLine($"{this.Id} Rented successfully :)");
+                IsAvailable = false;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("We're sorry the car is currently rented :(");
+                return false;
+            }
+        }
+
+        public bool Return()
+        {
+            if (IsAvailable == false)
+            {
+                Console.WriteLine($"{this.Id} Returned successfully :)");
+                IsAvailable = true;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The car is already available :(");
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Make)}: {Make}, {nameof(Model)}: {Model}, {nameof(RentalPrice)}: {RentalPrice}";
+        }
+    }
+}
---
+++ Car Rental System/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace Car_Rental_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car (1, "Totyota", "Corolla", 50, true),
+                new Car(2, "Honda", "Civic", 60, true),
+                new Car(3, "Ford", "Mustang", 80, true)
+            };
+
+
+            Console.WriteLine("Welcome to the car rental system!\n");
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------------------------------------");
+
+                foreach (var car in cars)
+                {
+                    if (car.IsAvailable)
+                    {
+                        Console.WriteLine($"{car} - Available.");
+                    }
+                    else if (!car.IsAvailable)
+                    {
+                        Console.WriteLine($"{car} - Rented.");
+                    }
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Rent a car");
+                Console.WriteLine("2. Return a car");
+                Console.WriteLine("3. View available cars");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("\nPlease enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("");
+                        Console.WriteLine("Rent a car:");
+                        Console.WriteLine("Please enter the ID of the car that you want to rent.");
+
+                        int carIdToRent = int.Parse(Console.ReadLine());
+
+                        foreach (var car in cars)
+                        {
+                            if (car.Id == carIdToRent)
+                            {
+                                car.Rent();
+                                
+                                break;
+                            }
+                            
+                        }
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Return a Car:");
+
+                        Console.Write("Please enter the ID of the car you want to return: ");
+                        int carIdToReturn = int.Parse(Console.ReadLine());
+                        
+                        foreach (Car car in cars)
+                        {
+                            if (car.Id == carIdToReturn)
+                            {
+                                car.Return();
+                                
+                                break;
+                            }
+                        }
+
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Available cars:");
+
+                        bool allCarsRented = true;
+                        foreach (Car car in cars)
+                        {
+                            if (car.IsAvailable)
+                            {
+                                Console.WriteLine(car + " - Available");
+                                allCarsRented = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine(car + " - Rented");
+                            }
+                        }
+
+                        if (allCarsRented)
+                        {
+                            Console.WriteLine("All cars are currently rented.\n");
+                        }
+
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Thank you for using the car rental system!");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.\n");
+                        
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
